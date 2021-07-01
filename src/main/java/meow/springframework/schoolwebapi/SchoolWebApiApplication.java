988ec19a358eaf81eab089182b4b1a43bdf97825
chetra-seng <sengchetra.love@icloud.com>
2021-07-01 09:45:37 +0700

    Initialized Spring

---
+++ src/main/java/meow/springframework/schoolwebapi/SchoolWebApiApplication.java
@@ -0,0 +1,13 @@
+package meow.springframework.schoolwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/meow/springframework/schoolwebapi/SchoolWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package meow.springframework.schoolwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
